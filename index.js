ecfb5e4df2aadc72992e27b67485164eeabe8441
Benjamin Cook <benjamin.cook@devmounta.in>
2022-06-29 11:49:38 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/', () => {
+    resizeBy.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const port = 4005
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
